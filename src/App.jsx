5ca684b567a7bc2c0a55bc98e746b4f02fc8354f
Shoaib Latif <shoaiblatif11@gmail.com>
2023-09-08 14:25:49 +0100

    Adjust css for website

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,15 @@ const App = () => (
     <section className="padding">
       SpecialOffer
     </section>
+    <section className="bg-pale-blue bg-clip padding">
+      CustomerReviews
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      Subscribe
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      Footer
+    </section>
   </main>
 );
 
