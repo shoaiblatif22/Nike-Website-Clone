e6a03790f02661cbe501052e34f4770a82d91965
Shoaib Latif <shoaiblatif11@gmail.com>
2023-09-20 00:46:58 +0100

    Styling navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,21 @@
+import { headerLogo } from "../assets/images";
+import { hamburger } from "../assets/icons";
+
 const Nav = () => {
   return (
-    <div>TEST</div>
+    <header>
+      <nav>
+        <a href="/">
+          <img 
+            src={headerLogo} 
+            alt="Logo"
+            width={130}
+            height={29}
+          
+          />
+        </a>
+      </nav>
+    </header>
   )
 }
 
