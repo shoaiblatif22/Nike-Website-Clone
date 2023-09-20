b5d11a27e221e2f3460b9c2a68e8ee66280a7f2c
Shoaib Latif <shoaiblatif11@gmail.com>
2023-09-20 15:37:01 +0100

    Added in further styling to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,9 @@
 
 const Button = ({label, iconURL}) => {
   return (
-    <button className="">
+    <button className="flex justify-center items-center gap-2
+    px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red
+    rounded-full text-white border-coral-red">
       {label}
       <img 
         src={iconURL}
