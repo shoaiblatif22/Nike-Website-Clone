ccdd889be9efbbda28531d91e734254a0005fb40
Shoaib Latif <shoaiblatif11@gmail.com>
2023-09-20 15:35:36 +0100

    Updated button to have label and iconurl passed in. Added in styling for button. Will adjust size and spacing next

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,13 @@
 
-const Button = () => {
+const Button = ({label, iconURL}) => {
   return (
     <button className="">
-      Click Me
+      {label}
+      <img 
+        src={iconURL}
+        alt="arrow right icon"
+        className="ml-2 rounded-full w-5 h-5"
+      />
     </button>
   )
 }
