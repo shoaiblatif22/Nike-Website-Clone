8ddfd4cb626869ff3775a0353a766b108bb87967
Shoaib Latif <shoaiblatif11@gmail.com>
2023-09-20 02:15:17 +0100

    Updated navbar component so that when the user decreases the screen size, the hamburger (menu) shows and disappears when the user increases screen size

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -27,6 +27,14 @@ const Nav = () => {
             </li>
           ))}
         </ul>
+        <div className="hidden max-lg:block">
+          <img
+            src={hamburger}
+            alt="Hamburger"
+            width={25}
+            height={25}
+          />
+        </div>
       </nav>
     </header>
   )
