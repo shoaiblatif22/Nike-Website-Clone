7c8051c52a5f307bd340097741cff1fe91461ce4
Shoaib Latif <shoaiblatif11@gmail.com>
2023-09-20 02:00:35 +0100

    Fixed error for navbar.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import { headerLogo } from "../assets/images";
 import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants";
 
 const Nav = () => {
   return (
@@ -15,7 +16,16 @@ const Nav = () => {
           />
         </a>
         <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
-
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-monthserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
