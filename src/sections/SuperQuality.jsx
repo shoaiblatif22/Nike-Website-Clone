c1a9a28f5b0446dd85011cec7c35e327f26358f0
Shoaib Latif <shoaiblatif11@gmail.com>
2023-09-22 18:21:41 +0100

    Adding in max width for text

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -8,7 +8,7 @@ const SuperQuality = () => {
       max-lg:flex-col gap-10 w-full max-container"
     >
       <div className="flex flex-1 flex-col">
-        <h2 className="mt-10 font-palanquin text-4xl capitalize font-bold">
+        <h2 className="mt-10 font-palanquin text-4xl capitalize font-bold lg:max-lg">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
           <br />
           <span className="text-coral-red inline-block mt-3">Nike
