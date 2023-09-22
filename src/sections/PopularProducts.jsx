ef3cc84ebb20de1d26ca7d1dd16fccbcec4a0c4d
Shoaib Latif <shoaiblatif11@gmail.com>
2023-09-22 16:59:34 +0100

    Added in colour to the word popular in title

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -8,7 +8,7 @@ const PopularProducts = () => {
       <div className='flex flex-col
       justify-start gap-5'>
         <h2 className='text-4xl font-palanquin 
-        font-bold'>Our <span>Popular</span>Products</h2>
+        font-bold'>Our <span className='text-coral-red'>Popular</span> Products</h2>
         <p>Experience top-notch quality and 
           style with our sought after selections.
         </p>
