e3c7dfee66775e9b6d39891a7f48cf21a6e6eeaa
Shoaib Latif <shoaiblatif11@gmail.com>
2023-09-22 01:02:45 +0100

    Added in background and flex-container for the three shoecards. Need to implement and ajust css so that it can be found underneat the main shoe object

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,17 +1,17 @@
 
 
 const ShoeCard = ({ imgURL, changeBigShoeImage, 
-  bigShoeImage}) => {
+  bigShoeImg}) => {
 
     const handleClick = () => {
-      if(bigShoeImage !== imgURL.bigShoe)
+      if(bigShoeImg !== imgURL.bigShoe)
       {
         changeBigShoeImage(imgURL.bigShoe)
       }
     }
   return (
     <div className={`border-2 rounded-xl
-      ${bigShoeImage === imgURL
+      ${bigShoeImg === imgURL
         ? 'border-coral-red'
         : 'border-transparent'
       } cursor-pointer max-sm:flex-1
@@ -19,6 +19,17 @@ const ShoeCard = ({ imgURL, changeBigShoeImage,
     `}
     onClick={handleClick}
     >
+      <div className="flex justify-center items-center
+      bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl
+      max-sm:p-4">
+        <img
+          src={imgURL.thumbnail}
+          alt="shoe collection"
+          width={127}
+          height={103}
+          className="object-contain"
+        />
+      </div>
    
 
     </div>
