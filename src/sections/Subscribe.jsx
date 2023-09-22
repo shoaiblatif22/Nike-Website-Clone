f2b247e12e57bd97a00b396a3c3d693bf8c536d6
Shoaib Latif <shoaiblatif11@gmail.com>
2023-09-23 00:40:33 +0100

    Added in content and classname for subscribe and newsletter section

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,6 +1,15 @@
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
+    <section className="max-container
+    flex justify-between items-center
+    max-lg:flex-col gap-10"
+    id="contact us">
+      <h3>Sign Up from 
+        <span>
+          Updates
+        </span> & Newsletter
+      </h3>
+    </section>
   )
 }
 
