d26d0b0b2ac312f2dc7154f2f71092e2bcd5c4f2
Shoaib Latif <shoaiblatif11@gmail.com>
2023-09-22 23:43:14 +0100

    adjusted css styling (copied from hero section and added in some content

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,4 +1,5 @@
 import { offer } from "../assets/images"
+import Button from "../components/Button"
 
 const SpecialOffer = () => {
   return (
@@ -12,6 +13,25 @@ const SpecialOffer = () => {
           className="object-contain w-full"
         />
       </div>
+
+      <div className="flex flex-1 flex-col">
+        <h2 className="mt-10 font-palanquin text-4xl capitalize font-bold lg:max-lg">
+          <span className="text-coral-red ">Special </span> Offer
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Embark on a shopping journey that redefines your experience width
+          unbeatable deals. From premier selections to incredible savings,
+          you could potentially save upto 30% off our shoes.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Navigate a realm of possibilites designed to fulfill your unique
+          desires, surpassing the loftiest expectations.
+        </p>
+        <div className="mt-11">
+          <Button label="View details" />
+        </div>
+      </div>
+
     </section>
   )
 }
