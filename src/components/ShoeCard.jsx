290e396aed4d6a3d3317d5b02cfbc3385cdb9f65
Shoaib Latif <shoaiblatif11@gmail.com>
2023-09-22 01:13:12 +0100

    Had to adjust hero. Img src had to be changed from bigShoe1 to bigShoeImg

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -11,7 +11,7 @@ const ShoeCard = ({ imgURL, changeBigShoeImage,
     }
   return (
     <div className={`border-2 rounded-xl
-      ${bigShoeImg === imgURL
+      ${bigShoeImg === imgURL.bigShoe
         ? 'border-coral-red'
         : 'border-transparent'
       } cursor-pointer max-sm:flex-1
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -47,7 +47,7 @@ const Hero = () => {
       <div className="relative flex-1 flex justify-center items-center xl:min-h-screen
       max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
         <img 
-          src={bigShoe1}
+          src={bigShoeImg}
           alt="shoe collection"
           width={610}
           height={500}
