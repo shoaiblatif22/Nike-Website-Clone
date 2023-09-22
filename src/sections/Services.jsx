35cfb2647c5f70f4550717d429f9792af1d1027d
Shoaib Latif <shoaiblatif11@gmail.com>
2023-09-22 20:08:24 +0100

    Added in section for Services and create a mapping function for the service card

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,6 +1,15 @@
+import { services } from '../constants'
+
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className="max-container flex justify-center
+    flex-wrap gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label}{...service} 
+        />
+      ))}
+
+    </section>
   )
 }
 
