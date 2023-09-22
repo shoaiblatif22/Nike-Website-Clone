0b0cf0aa7c742db03d9188edc9c453ac231cde3d
Shoaib Latif <shoaiblatif11@gmail.com>
2023-09-23 00:49:43 +0100

    Updated button and added in more styling to sign up button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 
-const Button = ({ backgroundColor, textColor, borderColor, label, iconURL }) => {
+const Button = ({ backgroundColor, textColor, borderColor, label, iconURL, fullWidth }) => {
   return (
     <button
       className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
         ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : 'bg-coral-red text-white border-coral-red'}
-        rounded-full`}
+        rounded-full ${fullWidth && 'w-full'}`}
     >
       {label}
 
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,14 +1,23 @@
+import Button from "../components/Button"
+
 const Subscribe = () => {
   return (
     <section className="max-container
     flex justify-between items-center
     max-lg:flex-col gap-10"
     id="contact us">
-      <h3>Sign Up from 
-        <span>
-          Updates
-        </span> & Newsletter
+      <h3 className="text-4xl leading-[68px] lg:max-w-md
+      font-palanquin font-bold">Sign Up from 
+        <span className="text-coral-red"> Updates </span> & Newsletter
       </h3>
+      <div className="lg:max-w-[40%]">
+        <input type="text"
+        placeholder="subscribe@nike.com"
+        className="input" />
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
+          <Button label="Sign Up"/>
+        </div>
+      </div>
     </section>
   )
 }
