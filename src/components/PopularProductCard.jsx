5baf68007d4d513a3d0c43f810f4483239676ed8
Shoaib Latif <shoaiblatif11@gmail.com>
2023-09-22 17:43:27 +0100

    Added in css including star rating. Added in import for star. Added in justify start rather than center as the styling looks far better on the left hand side

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,8 +1,20 @@
-const PopularProductCard = () => {
+import { star } from '../assets/icons'
+
+const PopularProductCard = ({ imgURL, name, price }) => {
   return (
-    <section>
-      
-    </section>
+    <div className="flex flex-1 
+    flex-col w-full mx-sm:w-full">
+      <img 
+        src={imgURL} alt={name}
+        className="w-[280px] h-[280px]" 
+      />
+      <div className="mt-8 flex justify-start gap-2.5">
+        <img src={star} alt="rating"
+        width={24} height={24}
+        />
+
+      </div>
+    </div>
   )
 }
 
