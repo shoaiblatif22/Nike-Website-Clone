377aa9c0fa884e3f929ac8fdc24ff5323b1473c1
Shoaib Latif <shoaiblatif11@gmail.com>
2023-09-23 00:36:49 +0100

    Added in more styling to h3 and p tags

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -13,16 +13,16 @@ const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
       <p className="mt-6 max-w-sm text-center info-text">
         {feedback}
       </p>
-      <div>
+      <div className="mt-3 flex justify-center items-center gap-2.5">
         <img
           src={star}
           width={24}
           height={24}
           className="object-contain m-0"
         />
-        <p>({rating})</p>
+        <p className="text-xl font-montserrat text-slate-gray">({rating})</p>
       </div>
-      <h3>{customerName}</h3>
+      <h3 className="mt-1 font-palanquin text-3xl text-center font-bold">{customerName}</h3>
     </div>
   )
 }
