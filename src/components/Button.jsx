656c9bdcb92ed6c95877cf43e4d8c570bf0efc31
Shoaib Latif <shoaiblatif11@gmail.com>
2023-09-22 18:42:06 +0100

    Since the icon wasn't rendering correctly (its not required anyway for the button) ive added in an if statement so that if statement to resolve this

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,11 +5,12 @@ const Button = ({label, iconURL}) => {
     px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red
     rounded-full text-white border-coral-red">
       {label}
-      <img 
+      
+      {iconURL && <img 
         src={iconURL}
         alt="arrow right icon"
         className="ml-2 rounded-full w-5 h-5"
-      />
+      />}
     </button>
   )
 }
