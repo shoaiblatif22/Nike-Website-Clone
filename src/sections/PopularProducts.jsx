b6feae911cc64f37a7443aac8e1351016e5d6776
Shoaib Latif <shoaiblatif11@gmail.com>
2023-09-22 17:07:47 +0100

    Added in further styling

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -9,9 +9,14 @@ const PopularProducts = () => {
       justify-start gap-5'>
         <h2 className='text-4xl font-palanquin 
         font-bold'>Our <span className='text-coral-red'>Popular</span> Products</h2>
-        <p>Experience top-notch quality and 
+        <p className='lg:max-w-lg mt-2 font-montserrat
+        text-slate-gray'>Experience top-notch quality and 
           style with our sought after selections.
         </p>
+      </div>
+
+      <div className='mt-16 grid lg:grid-cols-4 md:grid-cols-3 
+      sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14'>
 
       </div>
 
